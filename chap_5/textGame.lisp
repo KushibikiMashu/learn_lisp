db7e5dbc7c7aab6291980a5f5a1096755064657a
櫛引 実秀 <kushibiki@bengo4.com>
2018-10-31 21:43:20 +0900

    Add chap_5 and fizzbuzz on lisp

---
+++ chap_5/textGame.lisp
@@ -0,0 +1,52 @@
+;; 5.1
+;; define location and get to know where you are.
+(defparameter *nodes* '((living-room (you are in the living-room.
+							a wizard is snoring loudly on the couch.))
+                        (garden (you are in a beautiful garden.
+                                     there is a well in front of you.))
+                         (attic (you are in the attic.
+                                     there is a giant welding torch in the corner.))))
+
+(defun describe-location (location nodes)
+  (cadr (assoc location nodes)))
+
+;; 5.2
+;; define path
+(defparameter *edges* '((living-room (garden west door)
+                                     (attic upstairs ladder))
+                        (garden (living-room east door)
+                        (attic (living-room downstairs ladder)))))
+
+;; how to unquote to use code mode in a data mode
+(defun describe-path (edge)
+  `(there is a ,(caddr edge) going ,(cadr edge) from here.))
+
+(defun describe-paths (location edges)
+  (apply #'append (mapcar #'describe-path (cdr (assoc location edges)))))
+
+;; usage of mapcar
+(mapcar #'sqrt '(1 2 3 4 5)) ;; (1.0 1.4142135 1.7320508 2.0 2.236068)
+(mapcar #'car '((foo bar) (baz qux))) ;; (FOO BAZ)
+
+(defun describe-paths (location edges)
+  (apply #'append (mapcar #'describe-path (cdr (assoc location edges)))))
+
+;; 5.3
+;; objects
+(defparameter *objects* '(whiskey bucket frog chain))
+
+(defparameter *object-locations* '((whiskey living-room)
+                                   (bucket living-room)
+                                   (chain garden)
+                                   (frog garden)))
+
+(defun objects-at (loc objs obj-locs)
+  (labels ((at-loc-p (obj)
+         	(eq (cadr (assoc obj obj-locs)) loc)))
+      (remove-if-not #'at-loc-p objs)))
+
+(defun describe-objects (loc objs obj-locs)
+  (labels ((describe-obj (obj)
+                 `(you see a ,obj on the floor.)))
+      (apply #'append (mapcar #'describe-obj (objects-at loc objs obj-locs)))))
+
